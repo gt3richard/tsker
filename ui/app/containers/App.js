4d7e0fcf05d6b26aebd2f2740e2dbdc1f7f7a126
Richard Ali <richardali@richards-mbp.lan>
2019-04-14 11:52:21 -0700

    clean start

--- ui/app/containers/App.js
+++ ui/app/containers/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Tasks from '../components/Tasks';
-import TaskBar from '../components/TaskBar';
 import store from '../store/TskleyStore';
 
 
-export default class App extends React.Component<Props> {
-  props: Props;
+export default class App extends Component {
 
   render() {
     return (
       <React.Fragment>
-        <CssBaseline />
-        <TaskBar store={store} />
         <Tasks store={store} />
       </React.Fragment>
     );
--- ui/app/store/TskleyStore.js
+++ ui/app/store/TskleyStore.js
@@ -1,12 +1,33 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import {observable, action} from 'mobx';
+import {decorate, observable, action} from 'mobx';
+import axios from 'axios';
+
+const API = 'http://localhost:8001'
 
 class TskleyStore {
-    @observable teamName = 'Gold Team'
+    teamName = 'Gold Team'
+    user = {}
+    userName = ''
+
+    getUser(id) {
+      axios.get(`${API}/user?id=${id}`)
+      .then(response => {
+        console.log(response.data)
+        this.user = response.data
+        this.userName = response.data.name
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+    }
 }
 
-const store = new TskleyStore();
+decorate(TskleyStore, {
+  teamName: observable,
+  user: observable,
+  userName: observable,
+  getUser: action
+})
 
+const store = new TskleyStore();
 export default store
 export { TskleyStore };
